e9e59e8a1ca1126fc4f4b0445e495343e4661d8b
Javi <javier.aguilararribas7@gmail.com>
2017-05-24 16:44:02 +0200

    Borro la clase ahorcado y creo algunos atributos más.

--- src/Ahorcado/Partida.java
+++ src/Ahorcado/Partida.java
@@ -9,4 +9,45 @@ package Ahorcado;
  * 
  */
 public class Partida {
+	/**
+	 * 
+	 */
+	private String nombreJugador;
+	/**
+	 * 
+	 */
+	private int puntaje;
+	/**
+	 * 
+	 */
+	private int limiteFrases;
+	/**
+	 * 
+	 */
+	private int tiempoPorFrase;
+
+	/**
+	 * 
+	 */
+	public void mostrarProgreso() {
+	}
+
+	/**
+	 * 
+	 */
+	public void pedirLetra() {
+	}
+
+	/**
+	 * 
+	 */
+	public void resolver() {
+	}
+
+	/**
+	 * 
+	 * @return fin 
+	 */
+	public boolean comprobarFinal() {
+	}
 };
